9f5b985df0ae7b19b1e8ace679308c1d8cf4bf6c
lehaibg <41914498+lehaibg@users.noreply.github.com>
2019-06-03 00:16:43 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1vQ9ATXaFbXCeGC62mWlEtT1R7ZFKX56c'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1Y-EUw3M3PyfqjEpRezKk1t0qAZl3cKXH'
 export_file_name = 'export.pkl'
 
 classes = ['infected','notinfected']
